41fec8500c528d75354bfe01b6e31ddd90d146cc
eMobilis <chrispycharmprojects@gmail.com>
2023-03-28 13:11:19 +0300

    first commit

---
+++ app/src/main/java/com/example/chris/ANDROID.kt
@@ -0,0 +1,11 @@
+package com.example.chris
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ANDROID : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_android)
+    }
+}
